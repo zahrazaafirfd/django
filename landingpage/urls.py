fec669e987a9b0cfc5d9eb000ce3f8fab20d35dc
zahrazaafirfd <50177694+zahrazaafirfd@users.noreply.github.com>
2022-08-15 14:03:41 +0700

    push

--- landingpage/urls.py
+++ landingpage/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('admin/', admin.site.urls),
+    path('landingpage/', include('landingpage.urls')),
 ]
+
