fe920702c15a9376b6a055216e32ad47467ddeb3
zahrazaafirfd <50177694+zahrazaafirfd@users.noreply.github.com>
2022-08-15 13:38:58 +0700

    push

--- landingpage/apps.py
+++ landingpage/apps.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
+class LandingpageConfig(AppConfig):
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('landingpage/', include('landingpage.urls')),
